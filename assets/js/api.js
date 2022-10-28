a1f6811fafc5a561c268111fed2f013f0e6b348a
Vitor-HenriqueAS <vitorhenrique.a12@gmail.com>
2022-10-28 12:10:54 -0300

    update informations

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,7 @@
 
-function fetchProfileData(){
-	const url = ''
-	fetch(url)
+async function fetchProfileData(){
+	const url = 'https://raw.githubusercontent.com/Vitor-HenriqueAS/projeto-dio-potifolio/main/data/profile.json'
+	const fetching = await fetch(url)
+
+	return await fetching.json()
 }
---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+
+function updateProfileInfo(profileData) {
+	const photo = document.getElementById('profile.photo')
+	const name = document.getElementById('profile.name')
+	const job = document.getElementById('profile.job')
+	const location = document.getElementById('profile.location')
+	const email = document.getElementById('profile.email')
+
+	photo.src = profileData.photo
+	photo.alt = profileData.name
+
+	name.innerText = profileData.name
+	job.innerText = profileData.job
+	location.innerText = profileData.location
+
+	email.innerText = profileData.email
+	email.href = `mailto:${profileData.email}`
+}
+
+function updateSoftSkills(profileData) {
+	const softSkills = document.getElementById('profile.skills.softSkills')
+
+	softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
+function updateHardSkills(profileData) {
+	const hardSkills = document.getElementById('profile.skills.hardSkills')
+
+	hardSkills.innerHTML = profileData.skills.hardSkills.map(skill =>
+		`<li><img src='${skill.logo}' alt='${skill.name}' title='${skill.name}'></li>`).join('')
+}
+
+function updateLanguages(profileData) {
+	const languages = document.getElementById('profile.languages')
+
+	languages.innerHTML = profileData.languages.map(languages => `<li>${languages}</li>`).join('')
+}
+
+function updatePortfolio(profileData) {
+	const portfolio = document.getElementById('profile.portfolio')
+
+	portfolio.innerHTML = profileData.portfolio.map(project => {
+		return `
+						<li>
+							<h3 ${project.github ? 'class="github"' : ''}>
+								${project.name}
+							</h3>
+
+							<a href="${project.url}" target="_blank">${project.url}</a>
+						</li>
+					`
+	}).join('')
+}
+
+(async () => {
+
+	const profileData = await fetchProfileData()
+	updateProfileInfo(profileData)
+	updateSoftSkills(profileData)
+	updateHardSkills(profileData)
+	updateLanguages(profileData)
+	updatePortfolio(profileData)
+})()
